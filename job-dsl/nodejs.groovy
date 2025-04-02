8077c3714df4afdad571c55f7b7cb9af1b8f8a2e
Koushik Reddy T <116569709+koushikrdy@users.noreply.github.com>
2025-04-02 15:16:45 +0530

    Update nodejs.groovy

--- job-dsl/nodejs.groovy
+++ job-dsl/nodejs.groovy
@@ -1,12 +1,12 @@
 job('NodeJS example') {
     scm {
-        git('https://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
+        git('https://github.com/koushikrdy/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
             node / gitConfigName('DSL User')
             node / gitConfigEmail('jenkins-dsl@newtech.academy')
         }
     }
     triggers {
-        scm('H/5 * * * *')
+        scm('* 23 * * *')
     }
     wrappers {
         nodejs('nodejs') // this is the name of the NodeJS installation in 
